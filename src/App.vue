0f024d6de876db2dd55d0222f59a6ff41dc5c919
Yves Cabaneros <yvesjohnpaul@gmail.com>
2021-11-08 02:00:24 +0800

    sort and pass as props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="app">
+    <div class="container flex justify-center flex-wrap">
+      <Card v-for="villager in shuffle" :key="villager.index" :card="villager" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import Card from "./components/Card.vue";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      images: [
+        { img: "dom.png" },
+        { img: "filbert.png" },
+        { img: "judy.png" },
+        { img: "lily.png" },
+        { img: "lucky.png" },
+        { img: "sherb.png" },
+      ],
+      villagers: [],
+    };
+  },
+  mounted() {
+    this.villagers = [...this.images, ...this.images].map((villager, index) => ({
+        ...villager, matched: false, id: index
+      }));
+    console.log(this.villagers);
+  },
+  computed: {
+    shuffle() {
+      return this.villagers.sort(() => Math.random() - 0.5);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="
+      card
+      w-40
+      h-56
+      m-2
+      flex
+      justify-center
+      items-center
+      relative
+      cursor-pointer
+      hover:shadow-lg
+    "
+    :class="{'flipped' : active}"
+    @click="active = !active"
+  >
+    <img
+      :src="require(`../assets/${card.img}`)"
+      alt="ac"
+      class="h-full p-4 w-full front shadow-lg"
+      
+    />
+    <img
+      src="../assets/back.png"
+      alt="back"
+      class="h-full w-full back shadow-sm"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: ["card"],
+  data() {
+    return {
+      active: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.front {
+  transform: rotateY(90deg);
+  transition: all ease-in 0.2s;
+  position: absolute;
+}
+.flipped .front {
+  transform: rotateY(0deg);
+  transition-delay: 0.2s;
+}
+.back {
+  transition: all ease-in 0.2s;
+  transition-delay: 0.2s;
+}
+.flipped .back {
+  transform: rotateY(90deg);
+  transition-delay: 0s;
+}
+</style>
